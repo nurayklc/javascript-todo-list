77ccae6a861fdb92326dba4b90ac8948a6c6a678
nurayklc <nuriskl72@gmail.com>
2021-11-24 01:11:26 +0300

    Complete to do list.

--- js/todo-list.js
+++ js/todo-list.js
@@ -7,52 +7,59 @@ let todoList =
     ? JSON.parse(localStorage.getItem("task"))
     : [];
 
-const button = document.querySelector("#liveToastBtn");
-
-button.addEventListener("click", inputHandler);
-
-const alertEmpty = document.querySelector("#alertEmpty");
-
 // li elemeti oluşturma
 const liMarker = (text) => {
   const li = document.createElement("li");
   li.textContent = text;
   ul.appendChild(li);
+  let closeButton = document.createElement("span");
+  closeButton.textContent = "\u00D7";
+  closeButton.classList.add("close");
+  closeButton.onclick = removeTask;
+  li.append(closeButton);
+  ul.append(li);
 };
 
-function inputHandler(event) {
-  event.preventDefault();
-  if (input.value != null && input.value != KeyboardEvent.DOM_KEY_LOCATION_STANDARD) {
+function inputHandler() {
+  if (
+    input.value != null &&
+    input.value != KeyboardEvent.DOM_KEY_LOCATION_STANDARD
+  ) {
     newElement();
-    input.value = ""
+    input.value = "";
   } else {
-    $('.error').toast('show')
+    $(".error").toast("show");
   }
   input.value = "";
 }
 
 // yeni task ekleme
 function newElement() {
-  todoList.push(input.value);
-  $('.success').toast('show')
+  todoList.push(input.value)
+  $(".success").toast("show");
   localStorage.setItem("task", JSON.stringify(todoList));
   liMarker(input.value);
 }
 
 // listeleme
-todoList == null ? todoList : todoList.forEach((element) => {
-  liMarker(element);
-});
+todoList == null
+  ? todoList
+  : todoList.forEach((element) => {
+      liMarker(element);
+    });
 
 // yapıldı fonksiyonu
-ul.addEventListener("click", checkList)
-function checkList() {
-  ul.style.textDecoration = "line-through" 
-  let removeButton = document.createElement("button")
-  removeButton.innerHTML = "x"
-}
+ul.addEventListener("click", function checkList(e) {
+  e.target.classList.toggle("checked");
+});
 
 // delete fonksiyonu
-function removeTask() {
-  localStorage.removeItem("task",todoList)
+function removeTask(e) {
+  todoList = todoList.filter((x) => x !== e.target.previousSibling.textContent);
+  e.target.parentElement.remove();
+  saveToLocalStorage(todoList);
+}
+// sildikten sonra güncel listeyi kaydetme
+function saveToLocalStorage() {
+  localStorage.setItem("task", JSON.stringify(todoList));
 }
